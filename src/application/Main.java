d8b22f88fefd472e02a7707c5b31bba48ebd4dcd
Andrew on laptop <houghtonandrew0@gmail.com>
2017-08-02 17:30:28 +1000

    Linked the Single Game button to an method in the controller.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,52 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+
+	// Stage = window itself. Scene = content
+	private Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage=primaryStage;
+		mainWindow();
+//		primaryStage.show();
+	}
+	
+	public void mainWindow() {
+		//DISPLAYS all the elements in our window.
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//Connect to the view.
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainMenu.fxml"));
+			AnchorPane pane = loader.load();
+			
+			//Connect to controller
+			MainMenuController mainMenuController = loader.getController();
+			mainMenuController.setMain(this);
+			
+			//Create a new scene on the primary stage
+			Scene scene = new Scene(pane);
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+ 
--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,5 +1,17 @@
 package application;
 
-public class MainMenuController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+public class MainMenuController {
+	private Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	
+	public void handleSingleGame() {
+		System.out.println("test");
+	}
 }
