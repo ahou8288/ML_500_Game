9acb027187f5babc975982a06fffadea150f1a4e
Andrew on laptop <houghtonandrew0@gmail.com>
2017-08-03 15:33:35 +1000

    Cards now filled with some images

--- src/application/SingleGameController.java
+++ src/application/SingleGameController.java
@@ -12,37 +12,35 @@ public class SingleGameController {
 	private Main main;
 
 	//Card slots
-	@FXML private ImageView n0,n1,n2,n3,n4,n5,n6,n7,n8,n9;
-	@FXML private ImageView s0,s1,s2,s3,s4,s5,s6,s7,s8,s9;
-	@FXML private ImageView w0,w1,w2,w3,w4,w5,w6,w7,w8,w9;
-	@FXML private ImageView e0,e1,e2,e3,e4,e5,e6,e7,e8,e9;
+	@FXML private ImageView north_0,north_1,north_2,north_3,north_4,north_5,north_6,north_7,north_8,north_9;
+	@FXML private ImageView south_0,south_1,south_2,south_3,south_4,south_5,south_6,south_7,south_8,south_9;
+	@FXML private ImageView west_0,west_1,west_2,west_3,west_4,west_5,west_6,west_7,west_8,west_9;
+	@FXML private ImageView east_0,east_1,east_2,east_3,east_4,east_5,east_6,east_7,east_8,east_9;
 	
 	public void setMain(Main main) {
 		this.main = main;
 	}
 	
 	public void loadElements() {
-		List<ImageView> northImages = Arrays.asList(n0,n1,n2,n3,n4,n5,n6,n7,n8,n9);
-		List<ImageView> eastImages = Arrays.asList(e0,e1,e2,e3,e4,e5,e6,e7,e8,e9);
-		List<ImageView> westImages = Arrays.asList(w0,w1,w2,w3,w4,w5,w6,w7,w8,w9);
-		List<ImageView> southImages = Arrays.asList(s0,s1,s2,s3,s4,s5,s6,s7,s8,s9);
+		List<ImageView> northImages = Arrays.asList(north_0,north_1,north_2,north_3,north_4,north_5,north_6,north_7,north_8,north_9);
+		List<ImageView> southImages = Arrays.asList(south_0,south_1,south_2,south_3,south_4,south_5,south_6,south_7,south_8,south_9);
+		List<ImageView> eastImages = Arrays.asList(east_0,east_1,east_2,east_3,east_4,east_5,east_6,east_7,east_8,east_9);
+		List<ImageView> westImages = Arrays.asList(west_0,west_1,west_2,west_3,west_4,west_5,west_6,west_7,west_8,west_9);
 		
 		List<String> northExtensions = Arrays.asList("1c","2c","3c","4c","5c","6c","7c","8c","9c","10c");
-		loadSide(northImages,northExtensions,false);
-		loadSide(southImages,northExtensions,false);
-		loadSide(westImages,northExtensions,true);
-		loadSide(eastImages,northExtensions,true);
+		loadSide(northImages,northExtensions);
+		loadSide(southImages,northExtensions);
+		loadSide(eastImages,northExtensions);
+		loadSide(westImages,northExtensions);
 	}
 	
-	public void loadSide(List<ImageView> images, List<String> extensions, boolean rotate) {
+	public void loadSide(List<ImageView> images, List<String> extensions) {
 		String basePath = "images/";
 		String filetype = ".png";
 		for (int i=0;i<images.size();i++) {
 	        File file = new File(basePath+extensions.get(i)+filetype);
 	        Image image = new Image(file.toURI().toString());
 	        images.get(i).setImage(image);
-	        
-	        if (rotate) images.get(i).setRotate(90);
 		}
 	}
 }
